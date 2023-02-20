2261fdf48e9b0bfe1434deb5d23d280d9f72bd44
Germán Berná Martínez <gbm25@gcloud.ua.es>
2023-02-20 21:23:08 +0100

    tfg - perlin worm

--- core/src/model/World.java
+++ core/src/model/World.java
@@ -76,34 +76,38 @@ public class World {
             }
         }
 
-        int numCuevas = SIZE * SIZE * 256 / 8192;
-        int lengthCave1 = (int) ((Math.random() * (10 - 9)) + 9);
-        int lengthCave2 = (int) ((Math.random() * (7 - 2)) + 2);
+        System.out.println("Generando cuevas");
+        int SCALE = 30;
+        int OCTAVES = 4;
+        double PERSISTENCE = 0.5;
+        double LACUNARITY = 2.0;
+        PerlinWorm perlinWorm = new PerlinWorm(width , height, SCALE, OCTAVES, PERSISTENCE, LACUNARITY);
 
-        System.out.print("Generando cuevas");
-        for (int cueva = 1; cueva<numCuevas; cueva++) {
-            int heightCave = (int) ((Math.random() * (12 - 5)) + 5);
-            for (int x = (width / lengthCave1); x < (width / lengthCave2); x++) {
-                double heightLow = noise1.noise(x * 1.8, x * 1.8) / 8.0 - 5.0;
-                double heightHigh = noise2.noise(x * 1.6, x * 1.6) / 4.0 + 3.0;
-                double heightResult;
-                if (noise3.noise(x, x, 0.7, 2) / 8.0 > 0.0)
-                    heightResult = heightLow;
-                else
-                    heightResult = Math.max(heightHigh, heightLow);
-                heightResult /= 2.0;
-                if (heightResult < 0.0)
-                    heightResult = heightResult * 8.0 / 10.0;
-
-                int heightBlock = (int) Math.floor(heightResult + (double)heightCave);
-                heightBlock = Math.abs(heightBlock);
-                if(heightBlock > 10 && heightBlock < height -12 ){
-                    map[1][heightBlock][x] = Material.IRON.getId();
-                    //System.out.println("z: " + heightBlock + z + " x: " + x);
+        int[][] grid = perlinWorm.getGrid();
+        for (int x = 0; x < grid.length; x++) {
+            for (int z = 0; z < grid[0].length; z++) {
+                if (grid[x][z] == 1 && map[1][z][x] != 0 && map[1][z][x] != 2 && map[1][z][x] != 1 && map[1][z][x] != 13) {
+                    map[1][z][x] = Material.SKY.getId();
                 }
-
             }
         }
+
+        System.out.println ("Generando menas");
+        SCALE = 5;
+        OCTAVES = 4;
+        PERSISTENCE = 0.0001;
+        LACUNARITY = 1.0;
+        PerlinWorm perlinWorm2 = new PerlinWorm(width , height, SCALE, OCTAVES, PERSISTENCE, LACUNARITY);
+
+        grid = perlinWorm2.getGrid();
+        for (int x = 0; x < grid.length; x++) {
+            for (int z = 0; z < grid[0].length; z++) {
+                if (grid[x][z] == 1 && map[1][z][x] != 0 && map[1][z][x] != 2 && map[1][z][x] != 1 && map[1][z][x] != 13 && map[1][z][x] != 3) {
+                    map[1][z][x] = Material.IRON.getId();
+                }
+            }
+        }
+
         return this;
     }
 
@@ -130,7 +134,7 @@ public class World {
     public void dispose() {}
 
     public Material getMaterialByCoordinate(int layer, int col, int row) {
-        if (col < 0 || col >= getWidth() || row < 0 || row > getHeight())
+        if (col < 0 || col >= getWidth() || row < 0 || row > getHeight() || getHeight() - 1 - row < 0)
             return null;
 
         return Material.getMaterialById(map[layer][getHeight() - 1 - row ][col]);
@@ -150,5 +154,80 @@ public class World {
     }
     public long getSeed() {
         return seed;
+    }
+
+
+    public static class PerlinWorm {
+
+        private final int[][] grid;
+
+        public PerlinWorm(int width, int height, double scale, int octaves, double persistence, double lacunarity) {
+            this.grid = new int[width][height];
+
+            Random rng = new Random();
+            double offsetX = rng.nextDouble() * 1000;
+            double offsetY = rng.nextDouble() * 1000;
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    double noiseValue = 0;
+                    double amplitude = 1;
+                    double frequency = 1;
+                    for (int i = 0; i < octaves; i++) {
+                        double sampleX = (x + offsetX) / scale * frequency;
+                        double sampleY = (y + offsetY) / scale * frequency;
+                        double perlinValue = noise(sampleX, sampleY);
+                        noiseValue += perlinValue * amplitude;
+                        amplitude *= persistence;
+                        frequency *= lacunarity;
+                    }
+                    if (noiseValue > 0.3) {
+                        this.grid[x][y] = 1;
+                    }
+                }
+            }
+        }
+
+        private double noise(double x, double y) {
+            int X = (int) Math.floor(x) & 255;
+            int Y = (int) Math.floor(y) & 255;
+            x -= Math.floor(x);
+            y -= Math.floor(y);
+            double u = fade(x);
+            double v = fade(y);
+            int A = p[X] + Y;
+            int AA = p[A], AB = p[A + 1];
+            int B = p[X + 1] + Y;
+            int BA = p[B], BB = p[B + 1];
+            return lerp(v, lerp(u, grad(p[AA], x, y), grad(p[BA], x - 1, y)),
+                    lerp(u, grad(p[AB], x, y - 1), grad(p[BB], x - 1, y - 1)));
+        }
+
+        private double fade(double t) {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
+
+        private double lerp(double t, double a, double b) {
+            return a + t * (b - a);
+        }
+
+        private double grad(int hash, double x, double y) {
+            int h = hash & 3;
+            double u = h == 0 ? x : h == 1 ? y : x + y;
+            double v = h == 0 ? y : h == 1 ? x : y - x;
+            return ((hash & 4) == 0 ? 1 : -1) * (u + v);
+        }
+
+        private static final int[] p = new int[512];
+        static {
+            Random rng = new Random();
+            for (int i = 0; i < 256; i++) {
+                p[i] = p[i + 256] = rng.nextInt(256);
+            }
+        }
+
+        public int[][] getGrid() {
+            return this.grid;
+        }
     }
 }
